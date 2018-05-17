0dcf8bed2b67b4d7e5d0d76862db055e569d9fb6
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2018-05-17 11:59:03 +0200

    Updates

--- drat.R
+++ drat.R
@@ -13,14 +13,16 @@ example.drat = function() {
   drat::addRepo("skranz-repo")
   install.packages("restorepoint")
   
-  install.packages("stringtools",repos = "https://skranz-repo.github.io/drat/")
+  install.packages("restorepoint",repos = "https://skranz-repo.github.io/drat/")
+  
+  install.packages("restorepoint",repos = "file:D:/libraries/drat/drat")
 }
 
 insert.drat = function(pkg,repodir=getwd(),libdir, pkg.dir=file.path(libdir, pkg, pkg)) {
   
   dir = file.path(libdir, pkg, pkg)
   src = devtools::build(dir)
-  insertPackage(file, repodir)
+  insertPackage(src, repodir)
   bin = devtools::build(dir, binary = TRUE, args = c('--preclean'))
   insertPackage(bin, repodir)
  
