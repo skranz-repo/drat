855861b96d94f8cd347755ce97002dcb5fbf9061
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2018-11-15 10:06:51 +0100

    Add RTutorIncentiveContracts

--- drat.R
+++ drat.R
@@ -19,7 +19,11 @@ assignInNamespace("version_info", c(devtools:::version_info, list("3.5" = list(v
   insert.drat("dplyrExtras", repodir,libdir)
   insert.drat("regtools", repodir,libdir)
   insert.drat("RTutor", repodir,libdir, add.source = FALSE)
-  #drat::insertPackage("D:/libraries/drat/RTutor_2015.12.16.tar.gz", repodir)
+  #drat::insertPackage("D:/libraries/drat/RTutor_2018.06.08.tar.gz", repodir)
+  #
+  insert.drat("RTutorIncentiveContracts", repodir, libdir="D:/libraries/RTutor/examples/IncentiveContracts", add.binary=FALSE)
+  #drat::insertPackage("D:/libraries/drat/RTutorIncentiveContracts_0.1.tar.gz", repodir)
+  
   
   
   insert.drat("repgame", repodir, libdir)
